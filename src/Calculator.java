ff30514a6659ddaba2632e4b5badc2d2c500950a
Manan <mananparmar7042@gmail.com>
2021-08-25 20:53:38 +0530

    Empty String will return zero

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,9 @@
 
 public class Calculator {
 
-	
+	public Object calculate(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- src/MyJuint.java
+++ src/MyJuint.java
@@ -1,5 +1,12 @@
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
 
 public class MyJuint {
-	
+	@Test
+	public void emptyStringReturnsZero()
+	{
+		Calculator calculator=new Calculator();
+		assertEquals(calculator.calculate(""), 0);
+	}
 }
