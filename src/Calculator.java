3242eeeac739575e86d9ff765188f335896b2cc2
Manan <mananparmar7042@gmail.com>
2021-08-25 21:27:48 +0530

    Numbers Delimited by comma should be summed

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,10 +2,23 @@
 public class Calculator {
 
 	public int add(String input) {
+		String num[]=input.split(",");
 		if(input.isEmpty())
 			return 0;
-		return Integer.parseInt(input);
+		else if(num.length>1)
+		{
+			return sum(num);
+		}
+		return stringToInt(input);
 		
+	}
+	private int sum(String[] num)
+	{
+		return stringToInt(num[0])+stringToInt(num[1]);
+	}
+	private int stringToInt(String num)
+	{
+		return Integer.parseInt(num);
 	}
 
 }
--- src/MyJuint.java
+++ src/MyJuint.java
@@ -1,12 +1,27 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyJuint {
+	
+	private Calculator calculator;
+	
+	@Before
+	public void initialize()
+	{
+		calculator =new Calculator();
+	}
+	
 	@Test
 	public void emptyStringReturnsZero()
 	{
-		Calculator calculator=new Calculator();
 		assertEquals(calculator.add("1"), 1);
+	}
+	
+	@Test
+	public void commaDelimetedShouldBeSummed()
+	{
+		assertEquals(calculator.add("5,4"), 9);
 	}
 }
